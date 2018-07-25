2e10e664575752ee7f553379c21abdaeabf80e4a
Austin Herman <austin.j.herman@gmail.com>
2018-07-25 00:46:28 -0400

    CrawlerController

--- app/Http/Controllers/CrawlerController.php
+++ app/Http/Controllers/CrawlerController.php
@@ -7,8 +7,8 @@ use Goutte\Client as GoutteClient;
 class CrawlerController extends Controller {
 
   public function __construct() {
-    $this->allowedDomain = 'tilleeyecareassociates.com';
-    $this->startUrl = 'https://www.tilleeyecareassociates.com';
+    $this->allowedDomain = rtrim('tilleeyecareassociates.com', '/');
+    $this->startUrl = rtrim('https://www.tilleeyecareassociates.com/', '/');
     $this->processedUrls = [];
     $this->unprocessedUrls = [
       $this->startUrl
@@ -36,33 +36,35 @@ class CrawlerController extends Controller {
       $urlToProcess = $this->startUrl;
       $this->unprocessedUrls = [];
     }
+
     // otherwise process the url provided
     else {
       $crawler = $this->client->request('GET', $urlToProcess);
     }
 
-    // set the current URL as processed so we don't crawl forever
+    // set the current URL as processed
     $this->processedUrls[] = $urlToProcess;
 
     // find all <a> tags in DOM object
     $crawler->filter('a')->each(function ($node, $i) {
-      $href = $node->attr('href');
-      // remove trailing slash
+      $href = $node->link()->getUri();
       if ($href[-1] === '/') {
         $href = rtrim($href, '/');
       }
-      // 
+      // if <a> tag is within allowed domain, has not been queued for process, and has not 
+      // already been processed, then queue it for processing
       if (strpos($href, $this->allowedDomain) && !in_array($href, $this->unprocessedUrls) && !in_array($href, $this->processedUrls)) {
         $this->unprocessedUrls[] = $href;
       }
     });
 
-    // remove the processed url so we don't crawl forever
+    // remove current URL from unprocessed queue
     $key = array_search($urlToProcess, $this->unprocessedUrls);
     if ($key !== false) {
       unset($this->unprocessedUrls[$key]);
     }
 
+    // get links on next url queued for processing
     foreach($this->unprocessedUrls as $unprocessedUrl) {
       $this->findLinks($unprocessedUrl);
     }
