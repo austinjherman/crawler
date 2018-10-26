e7b77e31b4950cc2a5ad9e8ff53453a1c944a50f
Austin Herman <austin.j.herman@gmail.com>
2018-10-25 21:54:45 -0400

    add todos

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,28 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+$router->post('/users/authenticate', ['uses' => 'AuthController@login']);
 $router->post('/users/create', ['uses' => 'UserController@create']);
 $router->get('/users/{hash}', ['middleware' => 'auth', 'uses' => 'UserController@read']);
 $router->post('/users/update/{hash}', ['middleware' => 'auth', 'uses' => 'UserController@update']);
 $router->post('/users/delete/{hash}', ['middleware' => 'auth', 'uses' => 'UserController@delete']);
 
-// TODO
-// set expiry on tokens
-$router->post('/users/authenticate', ['uses' => 'AuthController@login']);
 $router->get('/protected', ['middleware' =>'auth', 'uses' => 'UserController@test']);
 
 $router->get('/crawl/urls', ['middleware' =>'auth', 'uses' => 'UrlCrawlerController@run']);
+
+// TODO
+
+// Should find all the links within a given domain and cache the responses
+//$router->get('/crawl', ['middleware' => 'auth', 'uses' => '']);
+
+// Should return a list of all sites that were crawled by the authenticated user
+//$router->get('/sites', ['middleware' =>'auth', 'uses' => '']);
+
+// Should return a summary of the requested domain, which should have been 
+// crawled already by the authenticated user
+//$router->get('/sites/{domain}', ['middleware' =>'auth', 'uses' => '']);
+
+// Should return a json-formatted list of pages for wordpress import
+//$router->get('/sites/{domain}/scrape', ['middleware' =>'auth', 'uses' => '']);
 
